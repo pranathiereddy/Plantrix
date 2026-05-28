6bd20134156f8f7f21dd514524a48ef27f396020
S. PRANATHI REDDY <pranathireddyserigudem@gmail.com>
2026-05-28 18:09:49 +0530

    commit

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
-weather_api_key = "171e97242c9b52994b1924b718af4b85"
+weather_api_key = ""
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
 MONGO_URI = os.environ.get("MONGO_URI")
